7e4436278e8c1bbb1491cdf0c9b9d81bca9447a1
Ahmadullah Hadi <aalp72142@gmail.com>
2025-10-28 23:10:20 +0600

    completed task scheduling and dashboard analytics

--- src/app/modules/review/review.controller.ts
+++ src/app/modules/review/review.controller.ts
@@ -3,6 +3,9 @@ import catchAsync from "../../shared/catchAsync"
 import sendResponse from "../../shared/sendResponse"
 import { IJWTPayload } from "../../types/common"
 import { ReviewService } from "./review.service"
+import pick from "../../helper/pick"
+import { reviewFilterableFields } from "./review.constant"
+import httpStatus  from "http-status"
 
 const insertIntoDB = catchAsync(async(req : Request & {user?:IJWTPayload}, res : Response) =>{
     const user = req.user
@@ -16,7 +19,21 @@ const insertIntoDB = catchAsync(async(req : Request & {user?:IJWTPayload}, res :
     })
 })
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, reviewFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortby', 'sortOrder']);
+    const result = await ReviewService.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Reviews retrieval successfully',
+        meta: result.meta,
+        data: result.data,
+    });
+});
+
 
 export const ReviewController ={
-    insertIntoDB
+    insertIntoDB,
+    getAllFromDB
 }
